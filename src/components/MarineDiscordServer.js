6b2f7a2beeb0fe10e83cfa938f394027ab807766
Kyrie Nguyen <sonnhthanglong@gmail.com>
2021-01-03 12:55:39 -0500

    Fix the date of Christmas wwww

--- src/components/MarineDiscordServer.js
+++ src/components/MarineDiscordServer.js
@@ -4,7 +4,7 @@ const common = "discord_server";
 const specialDates = {
     cirno: '9/9',
     halloween: '31/10',
-    christmas: '3/1'
+    christmas: '25/12'
 };
 const specialDate = Object.keys(specialDates).find(date => specialDates[date] === today);
 const discord_server = specialDate
